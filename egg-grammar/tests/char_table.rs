3235f14ffd6f610b7f58bd9a8143acce8476a0db
khai96_ <hvksmr1996@gmail.com>
2022-12-18 22:15:15 +0700

    test: Create `line_correctness`

---
+++ egg-grammar/tests/char_table.rs
@@ -0,0 +1,26 @@
+use egg_grammar::CharTable;
+use pretty_assertions::assert_eq;
+
+const SRC_TEXT: &str = concat! {
+    "Hello,\n",
+    "I ❤️ Rust 🦀,\r\n",
+    "So I use to create a programming language,\n\r",
+    "The language is called 'egg-shell' 🥚",
+};
+
+fn table() -> CharTable<impl Iterator<Item = char>> {
+    CharTable::from_static_str(SRC_TEXT).into_completed()
+}
+
+#[test]
+fn line_correctness() {
+    let table = table();
+    let received: Vec<_> = table
+        .loaded_line_list()
+        .iter()
+        .map(|(segment, eol)| (segment.to_string(), eol))
+        .collect();
+    dbg!(&received);
+    let expected = [];
+    assert_eq!(received, expected);
+}
