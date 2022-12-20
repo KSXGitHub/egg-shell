38cd028806c7b9d4f646fc476ad0a1629b00c805
khai96_ <hvksmr1996@gmail.com>
2022-12-20 20:35:52 +0700

    refactor: Rename a module

--- egg-grammar/src/lib.rs
+++ egg-grammar/src/lib.rs
@@ -1,15 +1,15 @@
-mod abstract_grid;
 mod char_cell;
 mod char_coord;
 pub mod char_grid;
+mod char_grid_access;
 mod eol;
 mod ordinal;
 pub mod text_slice;
 
-pub use abstract_grid::*;
 pub use char_cell::CharCell;
 pub use char_coord::CharCoord;
 pub use char_grid::CharGrid;
+pub use char_grid_access::*;
 pub use eol::EndOfLine;
 pub use ordinal::Ordinal;
 pub use text_slice::TextSliceDef;
