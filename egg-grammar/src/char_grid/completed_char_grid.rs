770e33b63a6e51e6b39fea402f71e1b01df726c4
khai96_ <hvksmr1996@gmail.com>
2022-12-22 14:51:35 +0700

    api: Implement [Load]CharAt for CompletedCharGrid

--- egg-grammar/src/char_grid/completed_char_grid.rs
+++ egg-grammar/src/char_grid/completed_char_grid.rs
@@ -1,6 +1,8 @@
 use super::CharGridLine;
-use crate::{CharCell, EndOfLine, TextSliceDef};
+use crate::{CharAt, CharCell, CharCoord, EndOfLine, LoadCharAt, TextSliceDef};
 use getset::{CopyGetters, Getters};
+use pipe_trait::Pipe;
+use thiserror::Error;
 
 /// Character grid with all characters loaded.
 #[derive(Clone, CopyGetters, Getters)]
@@ -23,5 +25,40 @@ impl CompletedCharGrid {
     pub fn line_list(&self) -> impl Iterator<Item = CharGridLine<'_, Self>> {
         let create = |(coord, eol)| CharGridLine::new(coord, eol, self);
         self.line_list.iter().copied().map(create)
+    }
+}
+
+/// Error type of [`CharAt`] and [`LoadCharAt`] for [`CompletedCharGrid`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Error)]
+pub enum CharAtError {
+    #[error("Line does not exist")]
+    LineOutOfBound,
+    #[error("Column does not exist")]
+    ColumnOutOfBound,
+}
+
+impl CharAt for CompletedCharGrid {
+    type Error = CharAtError;
+    fn char_at(&self, coord: CharCoord) -> Result<CharCell, CharAtError> {
+        let (line, _) = *self
+            .line_list
+            .get(coord.line.pred_count())
+            .ok_or(CharAtError::LineOutOfBound)?;
+        if coord.column.pred_count() > line.char_count() {
+            return Err(CharAtError::ColumnOutOfBound);
+        }
+        let char_pos = line.first_char_pos().advance_by(coord.column.pred_count());
+        self.char_list()
+            .get(char_pos.pred_count())
+            .copied()
+            .expect("char_pos should be within the range of char_list")
+            .pipe(Ok)
+    }
+}
+
+impl LoadCharAt for CompletedCharGrid {
+    type Error = CharAtError;
+    fn load_char_at(&mut self, coord: CharCoord) -> Result<CharCell, CharAtError> {
+        self.char_at(coord)
     }
 }
