f7f6548a4aa796d330c4708fb4884aa6a7fcddfd
khai96_ <hvksmr1996@gmail.com>
2022-12-29 19:43:11 +0700

    api(parser): Add stack

--- egg-parser/src/base.rs
+++ egg-parser/src/base.rs
@@ -1,33 +1,46 @@
 /// Success value of [`Parse::parse`].
 #[derive(Debug, Clone, Copy)]
-pub struct Response<Input, Output> {
+pub struct Response<Input, Output, Stack> {
+    /// Stack from parent parsers.
+    pub stack: Stack,
     /// Output emitted by the parser.
     pub output: Output,
     /// Remaining unparsed input.
     pub remaining: Input,
 }
 
-impl<Input, Output> Response<Input, Output> {
-    /// Create a [`Response`] from a tuple of [output](Response::output) and [remaining input](Response::remaining).
-    pub const fn from_tuple(output: Output, remaining: Input) -> Self {
-        Response { output, remaining }
+impl<Input, Output, Stack> Response<Input, Output, Stack> {
+    /// Create a [`Response`] from a tuple of [stack](Response::stack) from parent parsers,
+    /// [output](Response::output) of current parser, and [remaining unparsed input](Response::remaining).
+    pub const fn from_tuple(stack: Stack, output: Output, remaining: Input) -> Self {
+        Response {
+            stack,
+            output,
+            remaining,
+        }
     }
 
-    /// Convert the [`Response`] into a tuple of [output](Response::output) and [remaining input](Response::remaining).
-    pub fn into_tuple(self) -> (Output, Input) {
-        (self.output, self.remaining)
+    /// Convert the [`Response`] into a tuple of [stack](Response::stack) from parent parsers,
+    /// [output](Response::output) of current parser, and [remaining unparsed input](Response::remaining).
+    pub fn into_tuple(self) -> (Stack, Output, Input) {
+        (self.stack, self.output, self.remaining)
     }
 }
 
 /// Return type of [`Parse::parse`].
-pub type ParseResult<Input, Output, Error> = Result<Response<Input, Output>, Error>;
+pub type ParseResult<Input, Output, Stack, Error> = Result<Response<Input, Output, Stack>, Error>;
 
 /// Parse an input.
 pub trait Parse<Input> {
+    /// Stack to pass between parsers.
+    type Stack;
     /// Parsing result.
     type Output;
     /// Parsing error.
     type Error;
     /// Parse an input.
-    fn parse(input: Input) -> ParseResult<Input, Self::Output, Self::Error>;
+    fn parse(
+        parent_stack: Self::Stack,
+        input: Input,
+    ) -> ParseResult<Input, Self::Output, Self::Stack, Self::Error>;
 }
