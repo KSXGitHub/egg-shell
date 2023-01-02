d7f8cebc0eab19796f3fd02432313e1e16da02c6
khai96_ <hvksmr1996@gmail.com>
2023-01-02 22:09:25 +0700

    api: Slice by LineNumber

--- egg-text/src/char_grid/char_grid_slice_from.rs
+++ egg-text/src/char_grid/char_grid_slice_from.rs
@@ -50,6 +50,69 @@ where
     }
 }
 
+impl<BaseGrid> CharAt<CharCoord> for CharGridSliceFrom<BaseGrid, LineNumber>
+where
+    BaseGrid: CharAt<CharCoord>,
+{
+    type Char = BaseGrid::Char;
+    type Error = BaseGrid::Error;
+
+    fn char_at(self, coord: CharCoord) -> Result<Self::Char, Self::Error> {
+        let coord = coord.advance_line(self.start.pred_count());
+        self.grid.char_at(coord)
+    }
+}
+
+impl<BaseGrid> LineAt<LineNumber> for CharGridSliceFrom<BaseGrid, LineNumber>
+where
+    BaseGrid: LineAt<LineNumber>,
+{
+    type Line = BaseGrid::Line;
+    type Error = BaseGrid::Error;
+
+    fn line_at(self, ln_num: LineNumber) -> Result<Self::Line, Self::Error> {
+        let ln_num = self.start.advance_by(ln_num.pred_count());
+        self.grid.line_at(ln_num)
+    }
+}
+
+impl<BaseGrid> SliceFrom<LineNumber> for CharGridSliceFrom<BaseGrid, LineNumber>
+where
+    BaseGrid: SliceFrom<LineNumber>,
+{
+    type Slice = BaseGrid::Slice;
+    type Error = BaseGrid::Error;
+
+    fn slice_from(self, start: LineNumber) -> Result<Self::Slice, Self::Error> {
+        let start = self.start.advance_by(start.pred_count());
+        self.grid.slice_from(start)
+    }
+}
+
+impl<BaseGrid> SliceFrom<CharCoord> for CharGridSliceFrom<BaseGrid, LineNumber>
+where
+    BaseGrid: SliceFrom<CharCoord>,
+{
+    type Slice = BaseGrid::Slice;
+    type Error = BaseGrid::Error;
+
+    fn slice_from(self, start: CharCoord) -> Result<Self::Slice, Self::Error> {
+        let start = start.advance_column(self.start.pred_count());
+        self.grid.slice_from(start)
+    }
+}
+
+impl<BaseGrid> LineCount for CharGridSliceFrom<BaseGrid, LineNumber>
+where
+    BaseGrid: CharCount,
+{
+    fn line_count(&self) -> usize {
+        let total = self.grid.char_count();
+        let skipped = self.start.pred_count();
+        total - skipped
+    }
+}
+
 impl<BaseGrid> CharAt<CharCoord> for CharGridSliceFrom<BaseGrid, CharCoord>
 where
     BaseGrid: CharAt<CharCoord>,
