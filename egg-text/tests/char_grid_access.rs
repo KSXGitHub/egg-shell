8aa2426b3119cc87d04872009fd45212718ce1c5
khai96_ <hvksmr1996@gmail.com>
2023-01-03 21:15:46 +0700

    test: Track char.pos()

--- egg-text/tests/char_grid_access.rs
+++ egg-text/tests/char_grid_access.rs
@@ -40,6 +40,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(0, 0))
         .expect("char_at 1:1");
     assert_eq!(char.coord(), LnCol::from_pred_counts(0, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(0));
     assert_eq!(char.offset_from_ln_start(), 0);
     assert_eq!(char.offset_from_doc_start(), 0);
     assert_eq!(char.value(), &'H');
@@ -51,6 +52,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(0, 4))
         .expect("char_at 1:5");
     assert_eq!(char.coord(), LnCol::from_pred_counts(0, 4));
+    assert_eq!(char.pos(), CharPos::from_pred_count(4));
     assert_eq!(char.offset_from_ln_start(), 4);
     assert_eq!(char.offset_from_doc_start(), "Hell".len());
     assert_eq!(char.value(), &'o');
@@ -68,6 +70,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(1, 0))
         .expect("char_at 2:1");
     assert_eq!(char.coord(), LnCol::from_pred_counts(1, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(5 + 1));
     assert_eq!(char.offset_from_ln_start(), 0);
     assert_eq!(char.offset_from_doc_start(), "Hello,\n".len());
     assert_eq!(char.value(), &'I');
@@ -79,6 +82,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(1, 2))
         .expect("char_at 2:3");
     assert_eq!(char.coord(), LnCol::from_pred_counts(1, 2));
+    assert_eq!(char.pos(), CharPos::from_pred_count(5 + 1 + 2));
     assert_eq!(char.offset_from_ln_start(), 2);
     assert_eq!(char.offset_from_doc_start(), "Hello,\nI ".len());
     assert_eq!(char.value(), &'❤');
@@ -96,6 +100,10 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(3, 0))
         .expect("char_at 4:1");
     assert_eq!(char.coord(), LnCol::from_pred_counts(3, 0));
+    assert_eq!(
+        char.pos(),
+        CharPos::from_pred_count(5 + 1 + 12 + 1 + 43 + 1),
+    );
     assert_eq!(char.offset_from_ln_start(), 0);
     assert_eq!(char.offset_from_doc_start(), 74);
     assert_eq!(char.value(), &'T');
@@ -107,6 +115,10 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(3, 35))
         .expect("char_at 4:36");
     assert_eq!(char.coord(), LnCol::from_pred_counts(3, 35));
+    assert_eq!(
+        char.pos(),
+        CharPos::from_pred_count(5 + 1 + 12 + 1 + 43 + 1 + 35),
+    );
     assert_eq!(char.offset_from_ln_start(), 35);
     assert_eq!(char.offset_from_doc_start(), 109);
     assert_eq!(char.value(), &'🥚');
@@ -130,6 +142,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(0, 0))
         .expect("char_at 1:1");
     assert_eq!(char.coord(), LnCol::from_pred_counts(0, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(0));
     assert_eq!(char.offset_from_ln_start(), 0);
     assert_eq!(char.offset_from_doc_start(), 0);
     assert_eq!(char.value(), &'H');
@@ -141,6 +154,7 @@ fn lazy_char_at() {
         .char_at(LnCol::from_pred_counts(1, 2))
         .expect("char_at 2:3");
     assert_eq!(char.coord(), LnCol::from_pred_counts(1, 2));
+    assert_eq!(char.pos(), CharPos::from_pred_count(5 + 1 + 2));
     assert_eq!(char.offset_from_ln_start(), 2);
     assert_eq!(char.offset_from_doc_start(), "Hello,\nI ".len());
     assert_eq!(char.value(), &'❤');
