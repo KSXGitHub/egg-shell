2659b3d844b2c2013b3502a643413e39bd6d8ca6
khai96_ <hvksmr1996@gmail.com>
2023-01-06 13:38:29 +0700

    test: CharAt<CharPos> for LazyCharGrid

--- egg-text/tests/char_grid_access.rs
+++ egg-text/tests/char_grid_access.rs
@@ -1,7 +1,7 @@
 use egg_text::{
     char_grid::{completed_char_grid, lazy_char_grid},
-    CharAt, CharPos, CompletedCharGrid, EndOfLine, IterChar, IterLine, LazyCharGrid, LineAt, LnCol,
-    LnNum, TryIterChar, TryIterLine,
+    CharAt, CharOrEol, CharPos, CompletedCharGrid, EndOfLine, IterChar, IterLine, LazyCharGrid,
+    LineAt, LnCol, LnNum, TryIterChar, TryIterLine,
 };
 use pipe_trait::Pipe;
 use pretty_assertions::assert_eq;
@@ -158,6 +158,137 @@ fn lazy_char_at_ln_col() {
     assert_eq!(char.offset_from_ln_start(), 2);
     assert_eq!(char.offset_from_doc_start(), "Hello,\nI ".len());
     assert_eq!(char.value(), &'❤');
+    assert_eq!(grid.data().loaded_text(), SRC_TEXT);
+    assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
+}
+
+#[test]
+fn lazy_char_at_char_pos() {
+    let grid = partially_loaded_grid();
+
+    eprintln!("TEST (0)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(0))
+        .expect("char_at (0)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(0, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(0));
+    assert_eq!(char.offset_from_ln_start(), 0);
+    assert_eq!(char.offset_from_doc_start(), 0);
+    assert_eq!(char.value().to_string(), "H");
+    assert_eq!(grid.data().loaded_text(), "Hello,\nI ❤"); // preloaded from partially_loaded_grid
+    assert_eq!(grid.loaded_char_count(), 7); // preloaded from partially_loaded_grid
+
+    eprintln!("TEST (4)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(4))
+        .expect("char_at (4)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(0, 4));
+    assert_eq!(char.pos(), CharPos::from_pred_count(4));
+    assert_eq!(char.offset_from_ln_start(), 4);
+    assert_eq!(char.offset_from_doc_start(), "Hell".len());
+    assert_eq!(char.value().to_string(), "o");
+    assert_eq!(grid.data().loaded_text(), "Hello,\nI ❤"); // preloaded from partially_loaded_grid
+    assert_eq!(grid.loaded_char_count(), 7); // preloaded from partially_loaded_grid
+
+    eprintln!("TEST (6 + 1)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1))
+        .expect("char_at (6 + 1)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(1, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(6 + 1));
+    assert_eq!(char.offset_from_ln_start(), 0);
+    assert_eq!(char.offset_from_doc_start(), "Hello,\n".len());
+    assert_eq!(char.value().to_string(), "I");
+    assert_eq!(grid.data().loaded_text(), "Hello,\nI ❤️ Rust 🦀,\r\n");
+    assert_eq!(grid.loaded_char_count(), 20);
+
+    eprintln!("TEST (6 + 1 + 2)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1 + 2))
+        .expect("char_at (6 + 1 + 2)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(1, 2));
+    assert_eq!(char.pos(), CharPos::from_pred_count(6 + 1 + 2));
+    assert_eq!(char.offset_from_ln_start(), 2);
+    assert_eq!(char.offset_from_doc_start(), "Hello,\nI ".len());
+    assert_eq!(char.value().to_string(), "❤");
+    assert_eq!(grid.data().loaded_text(), "Hello,\nI ❤️ Rust 🦀,\r\n");
+    assert_eq!(grid.loaded_char_count(), 20);
+
+    eprintln!("TEST (6 + 1 + 12 + 1 + 45 + 1)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1))
+        .expect("char_at (6 + 1 + 12 + 1 + 45 + 1)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(3, 0));
+    assert_eq!(
+        char.pos(),
+        CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1),
+    );
+    assert_eq!(char.offset_from_ln_start(), 0);
+    assert_eq!(char.offset_from_doc_start(), 74);
+    assert_eq!(char.value().to_string(), "T");
+    assert_eq!(grid.data().loaded_text(), SRC_TEXT);
+    assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
+
+    eprintln!("TEST (6 + 1 + 12 + 1 + 45 + 1 + 35)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1 + 35))
+        .expect("char_at (6 + 1 + 12 + 1 + 45 + 1 + 35)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(3, 35));
+    assert_eq!(
+        char.pos(),
+        CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1 + 35),
+    );
+    assert_eq!(char.offset_from_ln_start(), 35);
+    assert_eq!(char.offset_from_doc_start(), 109);
+    assert_eq!(char.value().to_string(), "🥚");
+    assert_eq!(grid.data().loaded_text(), SRC_TEXT);
+    assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
+
+    eprintln!("TEST (6 + 1 + 12 + 1 + 45 + 1 + 35 + 1)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1 + 35 + 1))
+        .expect("char_at (6 + 1 + 12 + 1 + 45 + 1 + 35 + 1)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(3, 36));
+    assert_eq!(
+        char.pos(),
+        CharPos::from_pred_count(6 + 1 + 12 + 1 + 45 + 1 + 35 + 1),
+    );
+    assert_eq!(char.offset_from_ln_start(), 39);
+    assert_eq!(char.offset_from_doc_start(), SRC_TEXT.len());
+    assert_eq!(char.value().to_string(), "");
+    assert!(matches!(char.value(), CharOrEol::EndOfLine(EndOfLine::EOF)));
+    assert_eq!(grid.data().loaded_text(), SRC_TEXT);
+    assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
+
+    eprintln!("TEST (6 + 1 + 12 + 1 + 45 + 1 + 35 + 1 + 1)");
+    let error = grid
+        .char_at(CharPos::from_pred_count(
+            6 + 1 + 12 + 1 + 45 + 1 + 35 + 1 + 1,
+        ))
+        .expect_err("char_at (6 + 1 + 12 + 1 + 45 + 1 + 35 + 1 + 1)");
+    assert_eq!(error, lazy_char_grid::CharAtCharPosError::OutOfBound);
+
+    eprintln!("TEST (0) (again)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(0))
+        .expect("char_at (0)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(0, 0));
+    assert_eq!(char.pos(), CharPos::from_pred_count(0));
+    assert_eq!(char.offset_from_ln_start(), 0);
+    assert_eq!(char.offset_from_doc_start(), 0);
+    assert_eq!(char.value().to_string(), "H");
+    assert_eq!(grid.data().loaded_text(), SRC_TEXT);
+    assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
+
+    eprintln!("TEST (6 + 1 + 2) (again)");
+    let char = grid
+        .char_at(CharPos::from_pred_count(6 + 1 + 2))
+        .expect("char_at (6 + 1 + 2)");
+    assert_eq!(char.coord(), LnCol::from_pred_counts(1, 2));
+    assert_eq!(char.pos(), CharPos::from_pred_count(6 + 1 + 2));
+    assert_eq!(char.offset_from_ln_start(), 2);
+    assert_eq!(char.offset_from_doc_start(), "Hello,\nI ".len());
+    assert_eq!(char.value().to_string(), "❤");
     assert_eq!(grid.data().loaded_text(), SRC_TEXT);
     assert_eq!(grid.loaded_char_count(), SRC_TEXT.chars().count());
 }
