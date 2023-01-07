6bc6280ce428adbf69984328992be61fbbccf7d1
khai96_ <hvksmr1996@gmail.com>
2023-01-07 16:59:26 +0700

    api: Change how certain types are exposed

--- egg-text/src/char_grid.rs
+++ egg-text/src/char_grid.rs
@@ -1,11 +1,11 @@
 mod char_grid_common;
 pub mod char_grid_line;
-mod char_grid_slice_from;
+pub mod char_grid_slice_from;
 pub mod completed_char_grid;
 pub mod lazy_char_grid;
 
 use char_grid_common::*;
-pub use char_grid_line::*;
-pub use char_grid_slice_from::*;
+pub use char_grid_line::CharGridLine;
+pub use char_grid_slice_from::CharGridSliceFrom;
 pub use completed_char_grid::CompletedCharGrid;
 pub use lazy_char_grid::LazyCharGrid;
