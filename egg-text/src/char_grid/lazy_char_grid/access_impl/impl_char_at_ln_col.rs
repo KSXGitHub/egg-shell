cc2a51754706904627ada4b927d7097312f7d7ac
khai96_ <hvksmr1996@gmail.com>
2023-01-08 14:21:52 +0700

    refactor: Split a module into multiple parts

---
+++ egg-text/src/char_grid/lazy_char_grid/access_impl/impl_char_at_ln_col.rs
@@ -0,0 +1,59 @@
+use super::{LazyCharGrid, LineAtError, LoadCharError};
+use crate::{CharAt, CharCell, LineAt, LnCol, LnColOutOfBound};
+use derive_more::{Display, Error};
+use pipe_trait::Pipe;
+use std::fmt::Debug;
+
+/// Error type of [`CharAt<LnCol>`] for [`LazyCharGrid`].
+#[derive(Debug, Display, Clone, Copy, PartialEq, Eq, Error)]
+pub enum CharAtLnColError<IterError> {
+    /// An error occurred while loading a character.
+    LoadCharError(LoadCharError<IterError>),
+    /// The source iterator doesn't have enough lines to match the requested line index.
+    #[display(fmt = "Line does not exist")]
+    LineOutOfBound,
+    /// The line doesn't have enough characters to match the requested column index.
+    #[display(fmt = "Column does not exist")]
+    ColumnOutOfBound,
+}
+
+impl<IterError> TryFrom<CharAtLnColError<IterError>> for LnColOutOfBound {
+    type Error = LoadCharError<IterError>;
+    fn try_from(value: CharAtLnColError<IterError>) -> Result<Self, Self::Error> {
+        match value {
+            CharAtLnColError::LoadCharError(error) => Err(error),
+            CharAtLnColError::LineOutOfBound => Ok(LnColOutOfBound::LineOutOfBound),
+            CharAtLnColError::ColumnOutOfBound => Ok(LnColOutOfBound::ColumnOutOfBound),
+        }
+    }
+}
+
+impl<'a, IterError, CharIter> CharAt<LnCol> for &'a LazyCharGrid<CharIter>
+where
+    CharIter: Iterator<Item = Result<char, IterError>>,
+{
+    type Char = CharCell<char>;
+    type Error = CharAtLnColError<IterError>;
+
+    fn char_at(self, coord: LnCol) -> Result<CharCell<char>, CharAtLnColError<IterError>> {
+        let line = self.line_at(coord.line).map_err(|error| match error {
+            LineAtError::LoadCharError(error) => CharAtLnColError::LoadCharError(error),
+            LineAtError::OutOfBound => CharAtLnColError::LineOutOfBound,
+        })?;
+        if coord.column.pred_count() >= line.slice().char_count() {
+            return Err(CharAtLnColError::ColumnOutOfBound);
+        }
+        let char_pos = line
+            .slice()
+            .first_char_pos()
+            .advance_by(coord.column.pred_count());
+        self.data()
+            .loaded_char_list()
+            .get(char_pos.pred_count())
+            .copied()
+            .expect("char_pos should be within the range of char_list")
+            .pipe(Self::Char::try_from)
+            .expect("resulting char should not be an EOL")
+            .pipe(Ok)
+    }
+}
---
+++ egg-text/src/char_grid/lazy_char_grid/access_impl/impl_line_at_ln_num.rs
@@ -0,0 +1,43 @@
+use super::{LazyCharGrid, LoadCharError};
+use crate::{char_grid::CharGridLine, LineAt, LnNum, LnNumOutOfBound};
+use derive_more::{Display, Error};
+use std::fmt::Debug;
+
+/// Error type of [`LineAt`] for [`LazyCharGrid`].
+#[derive(Debug, Display, Clone, Copy, PartialEq, Eq, Error)]
+pub enum LineAtError<IterError> {
+    /// An error occurred while loading a character.
+    LoadCharError(LoadCharError<IterError>),
+    /// The source iterator doesn't have enough lines to match the requested index.
+    #[display(fmt = "Line does not exist")]
+    OutOfBound,
+}
+
+impl<IterError> TryFrom<LineAtError<IterError>> for LnNumOutOfBound {
+    type Error = LoadCharError<IterError>;
+    fn try_from(value: LineAtError<IterError>) -> Result<Self, Self::Error> {
+        match value {
+            LineAtError::LoadCharError(error) => Err(error),
+            LineAtError::OutOfBound => Ok(LnNumOutOfBound),
+        }
+    }
+}
+
+impl<'a, IterError, CharIter> LineAt<LnNum> for &'a LazyCharGrid<CharIter>
+where
+    CharIter: Iterator<Item = Result<char, IterError>> + 'a,
+{
+    type Line = CharGridLine<Self>;
+    type Error = LineAtError<IterError>;
+    fn line_at(self, ln_num: LnNum) -> Result<Self::Line, LineAtError<IterError>> {
+        while self.data().loaded_line_list.len() <= ln_num.pred_count()
+            && self.completion().is_incomplete()
+        {
+            self.load_line().map_err(LineAtError::LoadCharError)?;
+        }
+        if let Some((slice, eol)) = self.data().loaded_line_list.get(ln_num.pred_count()) {
+            return Ok(CharGridLine::new(*slice, *eol, self));
+        }
+        Err(LineAtError::OutOfBound)
+    }
+}
