df3166554362f64ee1f4128def882f04a4421903
khai96_ <hvksmr1996@gmail.com>
2023-01-08 20:52:00 +0700

    refactor: Reduce file tree nesting

--- egg-text/src/char_grid/lazy_char_grid.rs
+++ egg-text/src/char_grid/lazy_char_grid.rs
@@ -1,11 +1,20 @@
-mod access_impl;
 mod core_mtd;
 mod data;
+mod impl_char_at_char_pos;
+mod impl_char_at_ln_col;
+mod impl_line_at_ln_num;
+mod impl_slice_from;
+mod impl_try_iter_char;
+mod impl_try_iter_line;
 mod inner_text_slice;
 mod load_char;
 
-pub use access_impl::*;
 pub use core_mtd::*;
 pub use data::*;
+pub use impl_char_at_char_pos::*;
+pub use impl_char_at_ln_col::*;
+pub use impl_line_at_ln_num::*;
+pub use impl_try_iter_char::*;
+pub use impl_try_iter_line::*;
 pub use inner_text_slice::*;
 pub use load_char::*;
