a417d32f24d3d1776f9347d247e8f0611ce1adff
khai96_ <hvksmr1996@gmail.com>
2023-01-13 22:31:28 +0700

    api(text): Remove #[non_exhaustive]

--- egg-text/src/char_grid_error.rs
+++ egg-text/src/char_grid_error.rs
@@ -3,19 +3,16 @@ use derive_more::{Display, Error};
 /// The grid doesn't have enough characters to match the requested index.
 #[derive(Debug, Display, Clone, Copy, Error)]
 #[display(fmt = "Character position does not exist")]
-#[non_exhaustive]
 pub struct CharPosOutOfBound;
 
 /// The grid doesn't have enough lines to match the requested line index.
 #[derive(Debug, Display, Clone, Copy, Error)]
 #[display(fmt = "Line does not exist")]
-#[non_exhaustive]
 pub struct LnNumOutOfBound;
 
 /// The line doesn't have enough characters to match the requested column index.
 #[derive(Debug, Display, Clone, Copy, Error)]
 #[display(fmt = "Column does not exist")]
-#[non_exhaustive]
 pub struct ColNumOutOfBound;
 
 /// The grid does not contain the requested coordinate.
