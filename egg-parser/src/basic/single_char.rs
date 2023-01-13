191e6dfbfcd68ce1901ffe7e3a3903edcdb8ae2e
khai96_ <hvksmr1996@gmail.com>
2023-01-13 19:51:34 +0700

    refactor: Move Char to its own module

---
+++ egg-parser/src/basic/single_char.rs
@@ -0,0 +1,55 @@
+use crate::{Parse, ParseResult, Response, ResponseValue};
+use egg_text::{CharAt, CharCell, CharOrEol, CharPos, SliceFrom};
+use pipe_trait::Pipe;
+
+/// Parse a character.
+pub struct Char(pub char);
+
+/// Failure type of [`Char`].
+pub enum CharFailure {
+    /// Not the right character.
+    Mismatch(CharCell<CharOrEol>),
+}
+
+/// Error type of [`Char`].
+pub enum CharFatalError<CharAtError, SliceFromError> {
+    /// Error from [`CharAt`].
+    CharAt(CharAtError),
+    /// Error from [`SliceFrom`].
+    SliceFrom(SliceFromError),
+}
+
+impl<Input, Stack> Parse<Input, Stack> for Char
+where
+    Input: CharAt<CharPos, Char = CharCell<CharOrEol>> + SliceFrom<CharPos, Slice = Input> + Copy,
+{
+    type Failure = CharFailure;
+    type FatalError =
+        CharFatalError<<Input as CharAt<CharPos>>::Error, <Input as SliceFrom<CharPos>>::Error>;
+    type Output = CharCell<CharOrEol>;
+
+    fn parse(
+        self,
+        stack: Stack,
+        input: Input,
+    ) -> ParseResult<Input, Self::Output, Stack, Self::Failure, Self::FatalError> {
+        let output = input
+            .char_at(CharPos::from_pred_count(0))
+            .map_err(CharFatalError::CharAt)?;
+        if output.value() != &CharOrEol::Char(self.0) {
+            return output
+                .pipe(CharFailure::Mismatch)
+                .pipe(Response::Failure)
+                .pipe(Ok);
+        }
+        let remaining = input
+            .slice_from(CharPos::from_pred_count(1))
+            .map_err(CharFatalError::SliceFrom)?;
+        ResponseValue::builder()
+            .with_stack(stack)
+            .with_output(output)
+            .with_remaining(remaining)
+            .into_success()
+            .into_ok()
+    }
+}
