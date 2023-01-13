a1e7787b65906da11e46eedca558cdb86d87fbdd
khai96_ <hvksmr1996@gmail.com>
2023-01-13 16:16:14 +0700

    api(parser): Separate Failure from FatalError

--- egg-parser/src/base.rs
+++ egg-parser/src/base.rs
@@ -28,20 +28,29 @@ impl<Input, Output, Stack> Response<Input, Output, Stack> {
 }
 
 /// Return type of [`Parse::parse`].
-pub type ParseResult<Input, Output, Stack, Error> = Result<Response<Input, Output, Stack>, Error>;
+pub type ParseResult<Input, Output, Stack, Failure, FatalError> =
+    Result<Result<Response<Input, Output, Stack>, Failure>, FatalError>;
 
 /// Parse an input.
 pub trait Parse<Input> {
     /// Stack to pass between parsers.
     type Stack;
     /// Parsing result.
+    ///
+    /// The parser should return this value.
     type Output;
-    /// Parsing error.
-    type Error;
+    /// Parsing failure.
+    ///
+    /// Try again with other parser.
+    type Failure;
+    /// Irrecoverable error.
+    ///
+    /// The parser should terminate with this error if encounter.
+    type FatalError;
     /// Parse an input.
     fn parse(
         self,
         parent_stack: Self::Stack,
         input: Input,
-    ) -> ParseResult<Input, Self::Output, Self::Stack, Self::Error>;
+    ) -> ParseResult<Input, Self::Output, Self::Stack, Self::Failure, Self::FatalError>;
 }
--- egg-parser/src/basic.rs
+++ egg-parser/src/basic.rs
@@ -1,13 +1,18 @@
 use crate::{Parse, ParseResult, Response};
 use egg_text::{CharAt, CharCell, CharOrEol, CharPos, SliceFrom};
+use pipe_trait::Pipe;
 
 /// Parse a character.
 pub struct Char(pub char);
 
-/// Error type of [`Char`].
-pub enum CharError<CharAtError, SliceFromError> {
+/// Failure type of [`Char`].
+pub enum CharFailure {
     /// Not the right character.
     Mismatch(CharCell<CharOrEol>),
+}
+
+/// Error type of [`Char`].
+pub enum CharFatalError<CharAtError, SliceFromError> {
     /// Error from [`CharAt`].
     CharAt(CharAtError),
     /// Error from [`SliceFrom`].
@@ -19,27 +24,29 @@ where
     Input: CharAt<CharPos, Char = CharCell<CharOrEol>> + SliceFrom<CharPos, Slice = Input> + Copy,
 {
     type Stack = ();
-    type Error = CharError<<Input as CharAt<CharPos>>::Error, <Input as SliceFrom<CharPos>>::Error>;
+    type Failure = CharFailure;
+    type FatalError =
+        CharFatalError<<Input as CharAt<CharPos>>::Error, <Input as SliceFrom<CharPos>>::Error>;
     type Output = CharCell<CharOrEol>;
 
     fn parse(
         self,
         stack: Self::Stack,
         input: Input,
-    ) -> ParseResult<Input, Self::Output, Self::Stack, Self::Error> {
+    ) -> ParseResult<Input, Self::Output, Self::Stack, Self::Failure, Self::FatalError> {
         let output = input
             .char_at(CharPos::from_pred_count(0))
-            .map_err(CharError::CharAt)?;
+            .map_err(CharFatalError::CharAt)?;
         if output.value() != &CharOrEol::Char(self.0) {
-            return Err(CharError::Mismatch(output));
+            return output.pipe(CharFailure::Mismatch).pipe(Err).pipe(Ok);
         }
         let remaining = input
             .slice_from(CharPos::from_pred_count(1))
-            .map_err(CharError::SliceFrom)?;
-        Ok(Response {
+            .map_err(CharFatalError::SliceFrom)?;
+        Ok(Ok(Response {
             stack,
             output,
             remaining,
-        })
+        }))
     }
 }
