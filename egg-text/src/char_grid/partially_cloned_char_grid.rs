c8469ba21393dfc4db568f4ae24c218b640b1a95
khai96_ <hvksmr1996@gmail.com>
2023-01-21 18:06:01 +0700

    refactor: Remove pub(super)

--- egg-text/src/char_grid/partially_cloned_char_grid.rs
+++ egg-text/src/char_grid/partially_cloned_char_grid.rs
@@ -4,11 +4,11 @@ use getset::{CopyGetters, Getters};
 #[derive(Clone, CopyGetters, Getters)]
 pub struct PartiallyClonedCharGrid {
     #[getset(get_copy = "pub")]
-    pub(super) char_count: usize,
+    char_count: usize,
     #[getset(get = "pub")]
-    pub(super) text: String,
+    text: String,
     #[getset(get = "pub")]
-    pub(super) char_list: Vec<CharCell<CharOrEol>>, // TODO: reduce memory cost by storing only big characters.
+    char_list: Vec<CharCell<CharOrEol>>, // TODO: reduce memory cost by storing only big characters.
     #[getset(get = "pub")]
-    pub(super) line_list: Vec<(TextSliceDef, EndOfLine)>,
+    line_list: Vec<(TextSliceDef, EndOfLine)>,
 }
