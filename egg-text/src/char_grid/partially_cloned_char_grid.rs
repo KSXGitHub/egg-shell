4841455ef2d7c9ae5d19c77efe9ba8f7ed3caa05
khai96_ <hvksmr1996@gmail.com>
2023-01-21 18:10:11 +0700

    api(text): Change line type

--- egg-text/src/char_grid/partially_cloned_char_grid.rs
+++ egg-text/src/char_grid/partially_cloned_char_grid.rs
@@ -1,5 +1,11 @@
-use crate::{CharCell, CharOrEol, EndOfLine, TextSliceDef};
+use crate::{CharCell, CharOrEol};
 use getset::{CopyGetters, Getters};
+
+#[derive(Debug, Clone, Copy)]
+pub struct PartiallyClonedCharGridLineDef {
+    pub start_index: usize,
+    pub char_count: usize,
+}
 
 #[derive(Clone, CopyGetters, Getters)]
 pub struct PartiallyClonedCharGrid {
@@ -10,5 +16,5 @@ pub struct PartiallyClonedCharGrid {
     #[getset(get = "pub")]
     char_list: Vec<CharCell<CharOrEol>>, // TODO: reduce memory cost by storing only big characters.
     #[getset(get = "pub")]
-    line_list: Vec<(TextSliceDef, EndOfLine)>,
+    line_list: Vec<PartiallyClonedCharGridLineDef>,
 }
