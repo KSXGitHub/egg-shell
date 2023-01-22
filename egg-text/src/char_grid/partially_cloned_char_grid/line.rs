bf3d8c99a5971a4c9a2bec0f6e592c6657b1c5b2
khai96_ <hvksmr1996@gmail.com>
2023-01-22 14:41:03 +0700

    api(text): Add start_offset and size

--- egg-text/src/char_grid/partially_cloned_char_grid/line.rs
+++ egg-text/src/char_grid/partially_cloned_char_grid/line.rs
@@ -1,6 +1,10 @@
 /// Line definition.
 #[derive(Debug, Clone, Copy)]
 pub struct PartiallyClonedCharGridLineDef {
+    /// Starting character offset in string.
+    pub start_offset: usize,
+    /// Size of the line in bytes.
+    pub size: usize,
     /// Starting character index in character list.
     pub start_index: usize,
     /// Number of characters in the line.
--- egg-text/src/char_grid/partially_cloned_char_grid/push.rs
+++ egg-text/src/char_grid/partially_cloned_char_grid/push.rs
@@ -12,13 +12,17 @@ impl PartiallyClonedCharGrid {
             }
             CharOrEol::EndOfLine(eol) => {
                 self.text.push_str(eol.as_ref());
+                let start_offset = self.text.len();
                 let start_index = self.char_count;
-                let last_start_index = match self.line_list.last() {
-                    Some(last_line) => last_line.start_index,
-                    None => 0,
+                let (last_start_offset, last_start_index) = match self.line_list.last() {
+                    Some(last_line) => (last_line.start_offset, last_line.start_index),
+                    None => (0, 0),
                 };
+                let size = self.text.len() - last_start_offset;
                 let char_count = self.char_count - last_start_index;
                 self.line_list.push(PartiallyClonedCharGridLineDef {
+                    start_offset,
+                    size,
                     start_index,
                     char_count,
                 });
