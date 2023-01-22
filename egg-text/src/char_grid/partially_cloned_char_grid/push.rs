dad97b4f47883aba46741b723d8b63ca4eaa765b
khai96_ <hvksmr1996@gmail.com>
2023-01-22 14:48:25 +0700

    Fix off-by-one error

--- egg-text/src/char_grid/partially_cloned_char_grid/push.rs
+++ egg-text/src/char_grid/partially_cloned_char_grid/push.rs
@@ -4,21 +4,21 @@ use crate::{CharCell, CharOrEol};
 impl PartiallyClonedCharGrid {
     /// Add a character to the grid.
     pub fn push(&mut self, char: CharCell<CharOrEol>) {
-        self.char_count += 1;
-        self.char_list.push(char);
         match *char.value() {
             CharOrEol::Char(char) => {
                 self.text.push(char);
             }
             CharOrEol::EndOfLine(eol) => {
-                self.text.push_str(eol.as_ref());
                 let start_offset = self.text.len();
                 let start_index = self.char_count;
                 self.eol_list.push(PartiallyClonedCharGridEol {
                     start_offset,
                     start_index,
                 });
+                self.text.push_str(eol.as_ref());
             }
         }
+        self.char_count += 1;
+        self.char_list.push(char);
     }
 }
