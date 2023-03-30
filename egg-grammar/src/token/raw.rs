43b6602e2980ef1873f21583024a47aade3d6ad6
khai96_ <hvksmr1996@gmail.com>
2023-03-30 13:58:52 +0700

    api: RawToken::{Word,String}
    wip: Tokenizer

--- egg-grammar/src/token/raw.rs
+++ egg-grammar/src/token/raw.rs
@@ -1,2 +1,12 @@
 /// Token before reprocessing.
-pub enum RawToken {}
+pub enum RawToken {
+    /// Keyword or normal identifier.
+    Word,
+
+    /// String, exotic syntax, or abnormal identifier.
+    ///
+    /// * String could be `"abc"`, `'abc'`, `prefix"abc"`, or `prefix'abc'`.
+    /// * Exotic syntax could be `rg"[a-z]+"`, `gl"src/**/*.rs"`.
+    /// * Abnormal identifier could be `id"abc"`, `id'abc'`, `idl"egg: abc"`, or `idl'egg: abc'`.
+    String,
+}
