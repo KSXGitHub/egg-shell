fd31b4fb6e84c9ba440371935a4213d311167c76
khai96_ <hvksmr1996@gmail.com>
2023-04-19 21:50:36 +0700

    api: Attach `LnCol`
    wip: Tokenizer

--- egg-grammar/src/token/token_line.rs
+++ egg-grammar/src/token/token_line.rs
@@ -1,10 +1,11 @@
 use super::{ContentToken, IndentToken};
+use egg_ast::LnCol;
 
 /// List of tokens from a line.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TokenLine<Content> {
     /// Token of the indentation at the start of the line.
-    pub indent: IndentToken,
+    pub indent: (LnCol, IndentToken),
     /// List of [`ContentToken`] after indentation.
-    pub content: Vec<ContentToken<Content>>,
+    pub content: Vec<(LnCol, ContentToken<Content>)>,
 }
