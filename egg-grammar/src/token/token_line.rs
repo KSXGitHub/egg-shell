c2567524ac3dc215f771d621523e1d0e2b75d1b4
khai96_ <hvksmr1996@gmail.com>
2023-04-19 21:53:50 +0700

    api: Attach `LnNum`
    wip: Tokenizer

--- egg-grammar/src/token/token_line.rs
+++ egg-grammar/src/token/token_line.rs
@@ -1,9 +1,11 @@
 use super::{ContentToken, IndentToken};
-use egg_ast::LnCol;
+use egg_ast::{LnCol, LnNum};
 
 /// List of tokens from a line.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TokenLine<Content> {
+    /// The number of the line.
+    pub ln_num: LnNum,
     /// Token of the indentation at the start of the line.
     pub indent: (LnCol, IndentToken),
     /// List of [`ContentToken`] after indentation.
