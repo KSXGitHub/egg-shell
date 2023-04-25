12fe34ed958b4a7e824273cd0991b58650ac3e90
khai96_ <hvksmr1996@gmail.com>
2023-04-25 11:15:48 +0700

    doc: Return value

--- egg-grammar/src/token/parse_simple_token.rs
+++ egg-grammar/src/token/parse_simple_token.rs
@@ -1,5 +1,9 @@
 /// Parse a specific type of token.
 pub trait ParseSimpleToken<Content>: Sized {
     /// Parse the input text into a token.
+    ///
+    /// **Return value:**
+    /// * `None` means the token does not match.
+    /// * `Some((token, rest))` means that `token` is the token and `rest` is the remaining unparsed string.
     fn parse(input: Content) -> Option<(Self, Content)>;
 }
