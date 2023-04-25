f6e889b619b334e1da6e7c2dbf4480e816e8dc6f
khai96_ <hvksmr1996@gmail.com>
2023-04-25 11:13:27 +0700

    doc: Add a dot

--- egg-grammar/src/token/parse_simple_token.rs
+++ egg-grammar/src/token/parse_simple_token.rs
@@ -1,5 +1,5 @@
 /// Parse a specific type of token.
 pub trait ParseSimpleToken<Content>: Sized {
-    /// Parse the input text into a token
+    /// Parse the input text into a token.
     fn parse(input: Content) -> Option<(Self, Content)>;
 }
