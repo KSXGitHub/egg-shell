09d3004090085e10d722216e62d136be943554f2
khai96_ <hvksmr1996@gmail.com>
2023-04-25 18:26:04 +0700

    api: EmbedTokenTag
    wip: Tokenizer

--- egg-grammar/src/token/content/embed_token.rs
+++ egg-grammar/src/token/content/embed_token.rs
@@ -1,6 +1,14 @@
 /// Token for a chunk of embedded lines.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct EmbedToken<Content> {
-    pub tag: Content,
+    pub header: (EmbedTokenTag, Content),
     pub main_content: Vec<Content>,
+}
+
+/// Tag of an embed.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EmbedTokenTag {
+    Doc,
+    Exec,
+    String,
 }
