e838c3e0293ea6c5ba024eac48703d5e99b1260c
khai96_ <hvksmr1996@gmail.com>
2023-04-25 14:21:23 +0700

    api: ParseSimpleToken for CommentToken

--- egg-grammar/src/token/content/comment_token.rs
+++ egg-grammar/src/token/content/comment_token.rs
@@ -1,5 +1,29 @@
+use crate::token::ParseSimpleToken;
+
 /// Token for a line comment.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CommentToken<Content> {
     pub content: Content,
+}
+
+impl<Content> CommentToken<Content> {
+    /// If `content` is a comment, return a token in a [`Some`].
+    /// Otherwise, return a [`None`].
+    pub fn try_from_str(content: Content) -> Option<Self>
+    where
+        Content: AsRef<str>,
+    {
+        if content.as_ref().starts_with('#') {
+            Some(CommentToken { content })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> ParseSimpleToken<&'a str> for CommentToken<&'a str> {
+    fn parse(content: &'a str) -> Option<(Self, &'a str)> {
+        let token = CommentToken::try_from_str(content)?;
+        Some((token, ""))
+    }
 }
