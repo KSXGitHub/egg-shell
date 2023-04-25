a080a6ff931ed7a0883022027e480af9ee3b4e8b
khai96_ <hvksmr1996@gmail.com>
2023-04-25 11:16:19 +0700

    api: ParseSimpleToken for BracketToken
    wip: Tokenizer

--- egg-grammar/src/token/content/bracket_token.rs
+++ egg-grammar/src/token/content/bracket_token.rs
@@ -1,3 +1,5 @@
+use crate::token::ParseSimpleToken;
+
 /// Token for bracket characters.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct BracketToken {
@@ -23,4 +25,23 @@ pub enum BracketShape {
     Square,
     /// Either `{` or `}`.
     Curly,
+}
+
+impl<'a> ParseSimpleToken<&'a str> for BracketToken {
+    fn parse(input: &'a str) -> Option<(Self, &'a str)> {
+        use BracketDirection::*;
+        use BracketShape::*;
+        let (first, rest) = input.split_at(1);
+        let (direction, shape) = match first {
+            "(" => (Open, Round),
+            ")" => (Close, Round),
+            "[" => (Open, Square),
+            "]" => (Close, Square),
+            "{" => (Open, Curly),
+            "}" => (Close, Curly),
+            _ => return None,
+        };
+        let token = BracketToken { direction, shape };
+        Some((token, rest))
+    }
 }
