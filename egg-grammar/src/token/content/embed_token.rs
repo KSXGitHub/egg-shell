39bb27ece2965b9435a41a06bb4ffdc702bf837c
khai96_ <hvksmr1996@gmail.com>
2023-04-29 21:34:45 +0700

    api: Rename `QuoteType` to `Quote`
    wip: Tokenizer

--- egg-grammar/src/token/content/embed_token.rs
+++ egg-grammar/src/token/content/embed_token.rs
@@ -1,13 +1,13 @@
 /// Token for a chunk of embedded lines.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct EmbedToken<Content> {
-    pub header: (QuoteType, Content),
+    pub header: (Quote, Content),
     pub body: Vec<Content>,
 }
 
 /// Quote type of [`EmbedToken`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum QuoteType {
+pub enum Quote {
     /// Three single quotes (`'''`) were used to start the embedded block.
     Single,
     /// Three double quotes (`"""`) were used to start the embedded block.
--- egg-grammar/src/token/content/string_token.rs
+++ egg-grammar/src/token/content/string_token.rs
@@ -4,12 +4,12 @@ pub struct StringToken<Content> {
     pub prefix: Option<Content>,
     pub suffix: Option<Content>,
     pub body: Content,
-    pub quote: QuoteType,
+    pub quote: Quote,
 }
 
 /// Quote type of [`StringToken`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum QuoteType {
+pub enum Quote {
     /// Single quotes (`'`) were used to wrap the string content.
     Single,
     /// Double quotes (`"`) were used to wrap the string content.
