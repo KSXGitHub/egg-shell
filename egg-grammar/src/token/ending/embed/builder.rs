e6058fb3068dd3b4a6cf4f2d1ea912a7d8ec1dbf
khai96_ <hvksmr1996@gmail.com>
2023-05-11 21:32:57 +0700

    refactor: Rename a field

--- egg-grammar/src/token/ending/embed/builder.rs
+++ egg-grammar/src/token/ending/embed/builder.rs
@@ -3,6 +3,6 @@ use crate::token::IndentToken;
 
 #[derive(Debug, Clone)]
 pub struct EmbedTokenBuilder<'a, Tag, Attr, Body> {
-    header_indent: &'a IndentToken,
+    indent: &'a IndentToken,
     token: EmbedToken<Tag, Attr, Body>,
 }
