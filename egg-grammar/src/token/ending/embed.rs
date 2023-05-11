50431383837d81bc0e68575ba06ddf1f3bc564fd
khai96_ <hvksmr1996@gmail.com>
2023-05-11 19:17:00 +0700

    refactor: Group methods by trait bounds
    wip: Tokenizer

--- egg-grammar/src/token/ending/embed.rs
+++ egg-grammar/src/token/ending/embed.rs
@@ -13,13 +13,13 @@ pub struct EmbedToken<Tag, Attr, Body> {
     pub body: Vec<Body>,
 }
 
-impl<Tag, Attr, Body> EmbedToken<Tag, Attr, Body> {
+impl<'a, Tag, Attr, Body> EmbedToken<Tag, Attr, Body>
+where
+    Tag: ParseEmbedTokenTag<&'a str>,
+    Attr: ParseEmbedTokenAttr<&'a str>,
+{
     /// Parse the header of the embed token.
-    pub fn parse_header<'a>(input: &'a str) -> Option<(Tag, Attr)>
-    where
-        Tag: ParseEmbedTokenTag<&'a str>,
-        Attr: ParseEmbedTokenAttr<&'a str>,
-    {
+    pub fn parse_header(input: &'a str) -> Option<(Tag, Attr)> {
         let (tag, input) = Tag::parse(input)?;
         let attr = Attr::parse(input)?;
         Some((tag, attr))
@@ -28,21 +28,19 @@ impl<Tag, Attr, Body> EmbedToken<Tag, Attr, Body> {
     /// Start the parsing process with a header and an existing body.
     ///
     /// The existing body is typically an empty `Vec` constructed by `Vec::new` or `Vec::with_capacity`.
-    pub fn start_parsing<'a>(input: &'a str, body: Vec<Body>) -> Option<Self>
-    where
-        Tag: ParseEmbedTokenTag<&'a str>,
-        Attr: ParseEmbedTokenAttr<&'a str>,
-    {
+    pub fn start_parsing(input: &'a str, body: Vec<Body>) -> Option<Self> {
         let header = Self::parse_header(input)?;
         let token = EmbedToken { header, body };
         Some(token)
     }
+}
 
+impl<'a, Tag, Attr, Body> EmbedToken<Tag, Attr, Body>
+where
+    Body: ParseEmbedTokenBody<&'a str>,
+{
     /// Parse an item and add it to the body.
-    pub fn parse_body_item<'a>(&mut self, input: &'a str) -> Option<&'_ mut Self>
-    where
-        Body: ParseEmbedTokenBody<&'a str>,
-    {
+    pub fn parse_body_item(&mut self, input: &'a str) -> Option<&'_ mut Self> {
         let item = Body::parse(input)?;
         self.body.push(item);
         Some(self)
