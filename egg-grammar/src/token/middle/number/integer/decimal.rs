2da3f3fa1f4f47f60a731f4e968d878d5110587e
khai96_ <hvksmr1996@gmail.com>
2023-05-13 18:36:38 +0700

    api: DecimalToken
    wip: NumberToken
    
    wip: Tokenizer

---
+++ egg-grammar/src/token/middle/number/integer/decimal.rs
@@ -0,0 +1,64 @@
+use crate::token::ParseMiddleToken;
+
+/// Token for integer in base-10.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct DecimalToken<Content>(pub Content);
+
+const fn is_number_body(char: &char) -> bool {
+    matches!(char, '0'..='9' | '_')
+}
+
+impl<'a> ParseMiddleToken<&'a str> for DecimalToken<&'a str> {
+    fn parse(input: &'a str) -> Option<(Self, &'a str)> {
+        let mut iter = input.chars();
+
+        let first_char = iter.next()?;
+        if !first_char.is_ascii_digit() {
+            return None;
+        }
+
+        let tail_size: usize = iter.take_while(is_number_body).map(char::len_utf8).sum();
+        let end_offset = first_char.len_utf8() + tail_size;
+
+        let content = &input[..end_offset];
+        let remaining = &input[end_offset..];
+        let token = DecimalToken(content);
+        Some((token, remaining))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn positive() {
+        macro_rules! case {
+            ($input:literal -> $token:literal, $remaining:literal) => {
+                assert_eq!(
+                    DecimalToken::parse($input).unwrap(),
+                    (DecimalToken($token), $remaining),
+                )
+            };
+        }
+
+        case!("0" -> "0", "");
+        case!("123i32" -> "123", "i32");
+        case!("123_456_789u32" -> "123_456_789", "u32");
+        case!("123_suffix" -> "123_", "suffix");
+    }
+
+    #[test]
+    fn negative() {
+        macro_rules! case {
+            ($input:literal) => {
+                assert_eq!(DecimalToken::parse($input), None)
+            };
+        }
+
+        case!("");
+        case!("_123");
+        case!("u32");
+    }
+}
