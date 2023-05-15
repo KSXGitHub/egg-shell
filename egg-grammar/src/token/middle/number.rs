31cab3e423113614a39e7a6b824b034faa3ba77e
khai96_ <hvksmr1996@gmail.com>
2023-05-16 00:25:06 +0700

    test: Add more cases
    wip: NumberToken
    
    wip: Tokenizer

--- egg-grammar/src/token/middle/number.rs
+++ egg-grammar/src/token/middle/number.rs
@@ -49,10 +49,24 @@ mod test {
         }
 
         case!("0" -> DecimalToken("0"), None, "");
+        case!("0b" -> DecimalToken("0"), Some("b"), ""); // actual binary requires digits
+        case!("0o" -> DecimalToken("0"), Some("o"), ""); // actual octal requires digits
+        case!("0x" -> DecimalToken("0"), Some("x"), ""); // actual hexadecimal requires digits
         case!("123" -> DecimalToken("123"), None, "");
         case!("123u32" -> DecimalToken("123"), Some("u32"), "");
         case!("123_456u32" -> DecimalToken("123_456"), Some("u32"), "");
         case!("123.45f64" -> FractionalToken::new("123", Some("45"), None), Some("f64"), "");
+        case!("123." -> DecimalToken("123"), None, ".");
+        case!("123.f64" -> DecimalToken("123"), None, ".f64"); // by design, this is '(123).f64', not '123.0f64'
+        case!("123.45.rest" -> FractionalToken::new("123", Some("45"), None), None, ".rest");
+        case!("123+456" -> DecimalToken("123"), None, "+456");
+        case!("123.456,789" -> FractionalToken::new("123", Some("456"), None), None, ",789");
+        case!("0x123ABCi32 rest" -> HexadecimalToken("123ABC"), Some("i32"), " rest");
+        case!("123_456_789_suffix rest" -> DecimalToken("123_456_789_"), Some("suffix"), " rest");
+        case!("123.456e-789f64" -> FractionalToken::new("123", Some("456"), Some("-789")), Some("f64"), "");
+        case!("123.456-789f64" -> FractionalToken::new("123", Some("456"), None), None, "-789f64");
+        case!("54_083.850_96e+71_326f128" -> FractionalToken::new("54_083", Some("850_96"), Some("+71_326")), Some("f128"), "");
+        case!("54_083.850_96e+71_326f128+337" -> FractionalToken::new("54_083", Some("850_96"), Some("+71_326")), Some("f128"), "+337");
     }
 
     #[test]
