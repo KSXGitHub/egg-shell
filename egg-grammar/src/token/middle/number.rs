f5a5cf441a4cbd1beaafe9dc6e81fefe20b4bcbd
khai96_ <hvksmr1996@gmail.com>
2023-05-15 20:45:54 +0700

    test: NumberToken
    wip: NumberToken
    
    wip: Tokenizer

--- egg-grammar/src/token/middle/number.rs
+++ egg-grammar/src/token/middle/number.rs
@@ -29,3 +29,45 @@ impl<'a> ParseMiddleToken<&'a str> for NumberToken<&'a str> {
         Some((token, rest))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn positive() {
+        macro_rules! case {
+            ($input:literal -> $body:expr, $suffix:expr, $rest:literal) => {{
+                eprintln!("TEST: {:?}", $input);
+                let token = NumberToken {
+                    body: $body.into(),
+                    suffix: $suffix.map(NumberTokenSuffix),
+                };
+                assert_eq!(NumberToken::parse($input), Some((token, $rest)));
+            }};
+        }
+
+        case!("0" -> DecimalToken("0"), None, "");
+        case!("123" -> DecimalToken("123"), None, "");
+        case!("123u32" -> DecimalToken("123"), Some("u32"), "");
+        case!("123_456u32" -> DecimalToken("123_456"), Some("u32"), "");
+        case!("123.45f64" -> FractionalToken::new("123", Some("45"), None), Some("f64"), "");
+    }
+
+    #[test]
+    fn negative() {
+        macro_rules! case {
+            ($input:literal) => {
+                assert_eq!(NumberToken::parse($input), None)
+            };
+        }
+
+        case!("");
+        case!("_123");
+        case!("abc");
+        case!("i32");
+        case!("f64");
+        case!("-123"); // negative numbers are handled in semantic level, not token level
+    }
+}
