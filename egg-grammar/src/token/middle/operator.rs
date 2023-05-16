e89cdc4662b61bb12781b4846eb14294288a72b2
khai96_ <hvksmr1996@gmail.com>
2023-05-16 16:26:30 +0700

    api: Convert `OperatorToken` to a tuple struct
    wip: Tokenizer

--- egg-grammar/src/token/middle/operator.rs
+++ egg-grammar/src/token/middle/operator.rs
@@ -1,5 +1,3 @@
 /// Token a sequence of special characters.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct OperatorToken<Content> {
-    pub content: Content,
-}
+pub struct OperatorToken<Content>(pub Content);
