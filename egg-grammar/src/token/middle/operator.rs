b9a022008e47c805363f14c630a8dda9e6f5fc1b
khai96_ <hvksmr1996@gmail.com>
2023-05-17 11:20:45 +0700

    Allow $, #, and @ on operator body
    wip: Tokenizer

--- egg-grammar/src/token/middle/operator.rs
+++ egg-grammar/src/token/middle/operator.rs
@@ -4,16 +4,28 @@ use crate::token::ParseMiddleToken;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct OperatorToken<Content>(pub Content);
 
-const fn is_operator_char(char: &char) -> bool {
+const fn is_operator_head(char: &char) -> bool {
     matches!(char, '!' | '%' | '&' | '*'..='/' | ':'..='?' | '\\' | '^' | '|' | '~')
+}
+
+const fn is_operator_body(char: &char) -> bool {
+    is_operator_head(char) || matches!(char, '$' | '#' | '@')
 }
 
 impl<'a> ParseMiddleToken<&'a str> for OperatorToken<&'a str> {
     fn parse(input: &'a str) -> Option<(Self, &'a str)> {
-        let end_offset = input.chars().take_while(is_operator_char).count(); // ascii char has len_utf8 = 1
-        if end_offset == 0 {
+        let mut iter = input.chars();
+
+        let first_char = iter.next()?;
+        if !is_operator_head(&first_char) {
             return None;
         }
+
+        let first_char_len = 1; // because it is an ascii character.
+        debug_assert_eq!(first_char_len, first_char.len_utf8());
+        let tail_size = iter.take_while(is_operator_body).count(); // ascii char has len_utf8 = 1
+        let end_offset = first_char_len + tail_size;
+
         let content = &input[..end_offset];
         let rest = &input[end_offset..];
         let token = OperatorToken(content);
@@ -26,12 +38,12 @@ mod test {
     use super::*;
 
     #[test]
-    fn not_operator_chars() {
+    fn not_operator_head() {
         macro_rules! case {
             ($char:literal) => {{
                 let char = $char;
                 eprintln!("TEST: {char:?}");
-                assert!(!is_operator_char(&char));
+                assert!(!is_operator_head(&char));
             }};
 
             ($first:literal $($rest:literal)*) => {{
@@ -45,5 +57,24 @@ mod test {
         case!('$'); // used as variable interpolation in strings and macros
         case!('#'); // used in comments
         case!('@'); // used in annotations, meta attributes, and macros
+    }
+
+    #[test]
+    fn not_operator_body() {
+        macro_rules! case {
+            ($char:literal) => {{
+                let char = $char;
+                eprintln!("TEST: {char:?}");
+                assert!(!is_operator_body(&char));
+            }};
+
+            ($first:literal $($rest:literal)*) => {{
+                case!($first);
+                case!($($rest)*);
+            }}
+        }
+
+        case!('"' '\''); // used in string literals
+        case!('(' ')' '[' ']' '{' '}'); // used as bracket tokens
     }
 }
