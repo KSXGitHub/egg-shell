d52288fb71b0389c544dfced354ef87a3f0d64cf
khai96_ <hvksmr1996@gmail.com>
2023-05-18 14:44:49 +0700

    api: ParseMiddleToken for WordToken
    wip: Tokenizer

--- egg-grammar/src/token/middle/word.rs
+++ egg-grammar/src/token/middle/word.rs
@@ -1,4 +1,4 @@
-use crate::keyword::Keyword;
+use crate::{keyword::Keyword, token::ParseMiddleToken};
 use derive_more::{From, TryInto};
 
 /// Token of an identifier or a keyword.
@@ -64,5 +64,57 @@ where
 {
     fn as_ref(&self) -> &str {
         self.as_str()
+    }
+}
+
+const fn is_word_head(char: &char) -> bool {
+    matches!(char, 'a'..='z' | 'A'..='Z' | '_')
+}
+
+const fn is_word_body(char: &char) -> bool {
+    is_word_tail(char) || matches!(char, '-')
+}
+
+const fn is_word_tail(char: &char) -> bool {
+    is_word_head(char) || matches!(char, '0'..='9')
+}
+
+fn parse_word(input: &str) -> (&'_ str, &'_ str) {
+    let mut iter = input.chars();
+
+    let Some(first_char) = iter.next() else {
+        return ("", input);
+    };
+    if !is_word_head(&first_char) {
+        return ("", input);
+    }
+
+    let first_char_len = 1; // because it is an ascii character.
+    debug_assert_eq!(first_char_len, first_char.len_utf8());
+    let tail_size = iter.take_while(is_word_body).count(); // ascii char has len_utf8 = 1
+    let end_offset = first_char_len + tail_size;
+
+    let word = &input[..end_offset];
+    let last_char = word.chars().next_back().expect("word is not empty");
+
+    if is_word_tail(&last_char) {
+        let rest = &input[end_offset..];
+        (word, rest)
+    } else {
+        let end_offset = end_offset - 1; // it's ascii, no needs to worry about panic
+        let word = &input[..end_offset];
+        let rest = &input[end_offset..];
+        (word, rest)
+    }
+}
+
+impl<'a> ParseMiddleToken<&'a str> for WordToken<&'a str> {
+    fn parse(input: &'a str) -> Option<(Self, &'a str)> {
+        let (word, rest) = parse_word(input);
+        if word.is_empty() {
+            return None;
+        }
+        let token = WordToken::from(word);
+        Some((token, rest))
     }
 }
