e4e9d274d7093a2d49b031f721303f9125d77f02
khai96_ <hvksmr1996@gmail.com>
2023-05-20 17:26:35 +0700

    api: Make name in `DocTokenTag` optional
    wip: Tokenizer

--- egg-grammar/src/token/ending/embed/doc.rs
+++ egg-grammar/src/token/ending/embed/doc.rs
@@ -6,14 +6,17 @@ pub type DocToken<Content> =
 
 /// Tag of [`DocToken`].
 ///
-/// **Structure:** `@@<name>`
+/// **Structure:** `@@[name]`
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct DocTokenTag<Content>(pub WordToken<Content>);
+pub struct DocTokenTag<Content>(pub Option<WordToken<Content>>);
 
 impl<'a> ParseEmbedTokenTag<&'a str> for DocTokenTag<&'a str> {
     fn parse(input: &'a str) -> Option<(Self, &'a str)> {
         let input = input.strip_prefix("@@")?;
-        let (name, rest) = WordToken::parse(input)?;
+        let (name, rest) = match WordToken::parse(input) {
+            Some((name, rest)) => (Some(name), rest),
+            None => (None, input),
+        };
         let token = DocTokenTag(name);
         Some((token, rest))
     }
