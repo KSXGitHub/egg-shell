d46c2e677e80d8cecd31ef45eefc2081328b35b9
khai96_ <hvksmr1996@gmail.com>
2023-05-20 16:28:21 +0700

    api: ParseEmbedTokenTag for DocTokenTag
    wip: Tokenizer

--- egg-grammar/src/token/ending/embed/doc.rs
+++ egg-grammar/src/token/ending/embed/doc.rs
@@ -1,4 +1,4 @@
-use crate::token::RawToken;
+use crate::token::{ParseEmbedTokenTag, RawToken};
 
 /// Token for chunk of documentation lines.
 pub type DocToken<Content> = super::EmbedToken<DocTokenTag, RawToken<Content>, RawToken<Content>>;
@@ -6,3 +6,10 @@ pub type DocToken<Content> = super::EmbedToken<DocTokenTag, RawToken<Content>, R
 /// Tag of [`DocToken`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct DocTokenTag;
+
+impl<'a> ParseEmbedTokenTag<&'a str> for DocTokenTag {
+    fn parse(input: &'a str) -> Option<(Self, &'a str)> {
+        let rest = input.strip_prefix("@@")?;
+        Some((DocTokenTag, rest))
+    }
+}
