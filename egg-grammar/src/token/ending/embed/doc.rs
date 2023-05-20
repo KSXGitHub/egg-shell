2c53f5284bf8845e6bb8485357624016b3825ebc
khai96_ <hvksmr1996@gmail.com>
2023-05-20 16:38:21 +0700

    api: Add tag name to DocTokenTag
    wip: Tokenizer

--- egg-grammar/src/token/ending/embed/doc.rs
+++ egg-grammar/src/token/ending/embed/doc.rs
@@ -1,15 +1,18 @@
-use crate::token::{ParseEmbedTokenTag, RawToken};
+use crate::token::{ParseEmbedTokenTag, ParseMiddleToken, RawToken, WordToken};
 
 /// Token for chunk of documentation lines.
-pub type DocToken<Content> = super::EmbedToken<DocTokenTag, RawToken<Content>, RawToken<Content>>;
+pub type DocToken<Content> =
+    super::EmbedToken<DocTokenTag<Content>, RawToken<Content>, RawToken<Content>>;
 
 /// Tag of [`DocToken`].
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct DocTokenTag;
+pub struct DocTokenTag<Content>(pub WordToken<Content>);
 
-impl<'a> ParseEmbedTokenTag<&'a str> for DocTokenTag {
+impl<'a> ParseEmbedTokenTag<&'a str> for DocTokenTag<&'a str> {
     fn parse(input: &'a str) -> Option<(Self, &'a str)> {
-        let rest = input.strip_prefix("@@")?;
-        Some((DocTokenTag, rest))
+        let input = input.strip_prefix("@@")?;
+        let (name, rest) = WordToken::parse(input)?;
+        let token = DocTokenTag(name);
+        Some((token, rest))
     }
 }
