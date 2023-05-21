07dad6c106427de1bb73d8bab6c0c53b6d4dc6ad
khai96_ <hvksmr1996@gmail.com>
2023-05-21 21:41:42 +0700

    api: InsertWhitespaces for Vec<RawToken>
    wip: Tokenizer

--- egg-grammar/src/token/raw.rs
+++ egg-grammar/src/token/raw.rs
@@ -1,4 +1,4 @@
-use super::{ParseEmbedTokenAttr, ParseEmbedTokenBody};
+use super::{InsertWhitespaces, ParseEmbedTokenAttr, ParseEmbedTokenBody};
 use derive_more::{AsMut, AsRef, Deref, DerefMut, Display};
 use pipe_trait::Pipe;
 
@@ -15,5 +15,12 @@ impl<'a> ParseEmbedTokenAttr<&'a str> for RawToken<&'a str> {
 impl<'a> ParseEmbedTokenBody<&'a str> for RawToken<&'a str> {
     fn parse(input: &'a str) -> Option<Self> {
         input.pipe(RawToken).pipe(Some)
+    }
+}
+
+impl<'a> InsertWhitespaces<&'a str> for Vec<RawToken<&'a str>> {
+    fn insert_whitespaces(&mut self, ws: &'a str) -> Option<()> {
+        self.push(RawToken(ws));
+        Some(())
     }
 }
