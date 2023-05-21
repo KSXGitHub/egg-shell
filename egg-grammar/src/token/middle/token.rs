f4217f18cbe3ae7b2c778cc45822e7367a5aeb66
khai96_ <hvksmr1996@gmail.com>
2023-05-21 19:01:05 +0700

    test: MiddleToken
    wip: Tokenizer

--- egg-grammar/src/token/middle/token.rs
+++ egg-grammar/src/token/middle/token.rs
@@ -69,3 +69,92 @@ impl<'a> ParseMiddleToken<&'a str> for MiddleToken<&'a str> {
         None
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    macro_rules! def_macro {
+        ($name:ident -> $token_variant:ident) => {
+            /// This macro will not test the exact value of the token, only the top-level variant.
+            macro_rules! $name {
+                ($input:literal -> $rest:literal) => {{
+                    eprintln!();
+                    eprintln!("TEST: {:?}", $input);
+                    let (token, rest) = MiddleToken::parse($input).expect("Parse failure");
+                    dbg!(token);
+                    assert!(matches!(token, MiddleToken::$token_variant(_)));
+                    dbg!(rest);
+                    assert_eq!(rest, $rest);
+                }};
+            }
+        };
+    }
+
+    #[test]
+    fn string() {
+        def_macro!(case -> String);
+        case!("''" -> "");
+        case!("'abc def ghi'" -> "");
+        case!(r#""abc def ghi""# -> "");
+        case!("1foo-bar'abc def'" -> "");
+        case!(r"prefix'abc def \' ghi\n\t'suffix++' jkl mno'" -> "++' jkl mno'");
+        case!("GalaxyBrainGigachadWarStratagem'三十六計，走為上計'逃げるんだよ" -> "逃げるんだよ");
+        case!("'" -> "");
+        case!("\"" -> "");
+        case!("prefix'abc def ghi" -> "");
+    }
+
+    #[test]
+    fn word() {
+        def_macro!(case -> Word);
+        case!("print('hello world')" -> "('hello world')");
+        case!("if a + b == c then" -> " a + b == c then");
+    }
+
+    #[test]
+    fn number() {
+        def_macro!(case -> Number);
+        case!("123_456-789" -> "-789");
+        case!("123.45f64+789" -> "+789");
+        case!("123.45.rest" -> ".rest");
+        case!("0x123ABCi32 rest" -> " rest");
+    }
+
+    #[test]
+    fn bracket() {
+        def_macro!(case -> Bracket);
+        case!("(abc + 123)" -> "abc + 123)");
+        case!("[0, 1, 2, 3]" -> "0, 1, 2, 3]");
+        case!("{ a: 0, b: 1 }" -> " a: 0, b: 1 }");
+        case!(")" -> "");
+        case!("]" -> "");
+        case!("}" -> "");
+    }
+
+    #[test]
+    fn operator() {
+        def_macro!(case -> Operator);
+        case!("+=123" -> "123");
+        case!("+#abc" -> "abc");
+        case!("+ #abc" -> " #abc");
+        case!("$interpolation" -> "interpolation");
+        case!("@attribute" -> "attribute");
+    }
+
+    #[test]
+    fn negative() {
+        macro_rules! case {
+            ($input:literal) => {{
+                eprintln!("TEST: {:?}", $input);
+                assert_eq!(MiddleToken::parse($input), None);
+            }};
+        }
+
+        case!("");
+        case!(" ");
+        case!("\t");
+        case!("# this is a comment");
+    }
+}
