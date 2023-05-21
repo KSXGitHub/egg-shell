f6dab0c267c05bd191a9eed03ab3f40621cbefd2
khai96_ <hvksmr1996@gmail.com>
2023-05-21 22:13:42 +0700

    api: ParseEmbedTokenTag for TextTokenTag
    wip: Tokenizer

--- egg-grammar/src/token/ending/embed/text.rs
+++ egg-grammar/src/token/ending/embed/text.rs
@@ -1,4 +1,4 @@
-use crate::token::RawToken;
+use crate::token::{ParseEmbedTokenTag, RawToken};
 
 /// Token of multi-line string.
 pub type TextToken<Content> = super::EmbedToken<TextTokenTag, RawToken<Content>, RawToken<Content>>;
@@ -10,4 +10,19 @@ pub enum TextTokenTag {
     Single,
     /// Three double quotes (`"""`) were used to start the embedded block.
     Double,
+}
+
+impl<'a> ParseEmbedTokenTag<&'a str> for TextTokenTag {
+    fn parse(input: &'a str) -> Option<(Self, &'a str)> {
+        macro_rules! try_parse {
+            ($syntax:literal -> $token_variant:ident) => {
+                if let Some(rest) = input.strip_prefix($syntax) {
+                    return Some((TextTokenTag::$token_variant, rest));
+                }
+            };
+        }
+        try_parse!("'''" -> Single);
+        try_parse!("\"\"\"" -> Double);
+        None
+    }
 }
