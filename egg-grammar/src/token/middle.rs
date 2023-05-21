36fe39613496b05aff84aec35e4a27050a88d84e
khai96_ <hvksmr1996@gmail.com>
2023-05-21 15:07:52 +0700

    refactor: Move `MiddleToken` out of index file
    wip: Tokenizer

--- egg-grammar/src/token/middle.rs
+++ egg-grammar/src/token/middle.rs
@@ -2,60 +2,12 @@ pub mod bracket;
 pub mod number;
 mod operator;
 pub mod string;
+mod token;
 pub mod word;
 
 pub use bracket::BracketToken;
 pub use number::NumberToken;
 pub use operator::OperatorToken;
 pub use string::StringToken;
+pub use token::MiddleToken;
 pub use word::WordToken;
-
-/// Token in the middle of the line.
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum MiddleToken<Content> {
-    /// Keyword or normal identifier.
-    Word(WordToken<Content>),
-
-    /// String, exotic syntax, or abnormal identifier.
-    ///
-    /// **Including**
-    ///
-    /// * String: `"abc"`, `'abc'`, `prefix"abc"`, `prefix'abc'`, etc.
-    /// * Exotic syntax: `rg"[a-z]+"`, `gl"src/**/*.rs"`, etc.
-    /// * Abnormal identifier: `id"abc"`, `id'abc'`, `idl"egg: abc"`, `idl'egg: abc'`, etc.
-    ///
-    /// **Excluding**
-    ///
-    /// * Multi-line string.
-    String(StringToken<Content>),
-
-    /// Number.
-    ///
-    /// **Including**
-    ///
-    /// * Positive decimal integer: `123`, `123i32`, `123_456_789`, etc.
-    /// * Positive hexadecimal integer: `0x1234ABCD`, `0x1234abcd`, `0x1234ABCD_i32`, etc.
-    /// * Positive floating point: `123.45`, `123.45f`, `123.45f32`, etc.
-    ///
-    /// **Excluding**
-    ///
-    /// * Negative numbers. Composing `-` and number instead.
-    /// * `nan` and `inf`. They are keywords.
-    Number(NumberToken<Content>),
-
-    /// Round bracket, square bracket, curly bracket, open or closed.
-    Bracket(BracketToken),
-
-    /// A sequence of special characters.
-    ///
-    /// **Including**
-    ///
-    /// * Expression operator: `+`, `-`, `*`, `/`, `<`, `<=`, `>`, `>=`, `==`, `!=`, etc.
-    /// * Path separator (`::`).
-    /// * Dot notation (`.`).
-    /// * Type specifier (`:`).
-    /// * Binding or assignment (`=`).
-    /// * etc.
-    Operator(OperatorToken<Content>),
-}
