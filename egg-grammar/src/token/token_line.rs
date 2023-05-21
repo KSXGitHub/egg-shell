f3ef41d029a6e9e95e278fefc8ab83bce3cb09c9
khai96_ <hvksmr1996@gmail.com>
2023-05-21 20:00:03 +0700

    api: Fix coord type
    wip: Tokenizer

--- egg-grammar/src/token/token_line.rs
+++ egg-grammar/src/token/token_line.rs
@@ -1,5 +1,5 @@
 use super::{EndingToken, IndentToken, MiddleToken};
-use egg_ast::{LnCol, LnNum};
+use egg_ast::{ColNum, LnNum};
 
 /// List of tokens from a line.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -7,9 +7,9 @@ pub struct TokenLine<Content> {
     /// The number of the line.
     pub ln_num: LnNum,
     /// Token of the indentation at the start of the line.
-    pub indent: (LnCol, IndentToken),
+    pub indent: (ColNum, IndentToken),
     /// List of [`MiddleToken`] after indentation.
-    pub middle: Vec<(LnCol, MiddleToken<Content>)>,
+    pub middle: Vec<(ColNum, MiddleToken<Content>)>,
     /// Optional [`EndingToken`] at the end of the line.
-    pub ending: (LnCol, Option<EndingToken<Content>>),
+    pub ending: (ColNum, Option<EndingToken<Content>>),
 }
