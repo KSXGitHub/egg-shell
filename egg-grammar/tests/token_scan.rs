b29ef9a3d48ff861d33cfb262890ca05c6220f9f
khai96_ <hvksmr1996@gmail.com>
2023-05-22 23:08:00 +0700

    test: TokenLine::ln_text
    wip: Test

--- egg-grammar/tests/token_scan.rs
+++ egg-grammar/tests/token_scan.rs
@@ -10,9 +10,19 @@ macro_rules! test_snapshot {
     }};
 }
 
+macro_rules! test_ln_text {
+    ($tokens:expr, $text:expr) => {{
+        eprintln!("TEST: source of each TokenLine");
+        let received: Vec<_> = $tokens.iter().map(|item| item.ln_text).collect();
+        let expected: Vec<_> = $text.lines().collect();
+        assert_eq!(&received, &expected);
+    }};
+}
+
 #[test]
 fn hello_world() {
     let text = include_str!("fixtures/hello-world.egg");
     let tokens: Vec<_> = dbg!(Scan::new(text).collect());
     test_snapshot!(tokens, "snapshots/token-scan/hello-world.txt");
+    test_ln_text!(tokens, text);
 }
