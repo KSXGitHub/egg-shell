3e5cbae2c12281d0d7ffc10170e410c052ce04c2
khai96_ <hvksmr1996@gmail.com>
2023-05-23 15:37:42 +0700

    test: refactor: Rewrite some macros into functions
    wip: Test

--- egg-grammar/tests/token_scan.rs
+++ egg-grammar/tests/token_scan.rs
@@ -1,33 +1,29 @@
 use egg_grammar::token::{Scan, TokenLine};
 use pretty_assertions::assert_eq;
 
-macro_rules! title {
-    ($title:literal) => {{
-        eprintln!();
-        eprintln!("TEST: {}", $title);
-    }};
+fn title(title: &str) {
+    eprintln!();
+    eprintln!("TEST: {}", title);
 }
 
 macro_rules! test_snapshot {
     ($tokens:expr, $path:literal) => {{
-        title!("debug format snapshot");
+        title("debug format snapshot");
         let received = format!("{:#?}", &$tokens);
         let expected = include_str!($path);
         assert_eq!(received.trim(), expected.trim());
     }};
 }
 
-macro_rules! test_ln_text {
-    ($tokens:expr, $text:expr) => {{
-        title!("source of each TokenLine");
-        let received: Vec<_> = $tokens
-            .iter()
-            .flat_map(TokenLine::all_src_text)
-            .copied()
-            .collect();
-        let expected: Vec<_> = $text.lines().collect();
-        assert_eq!(&received, &expected);
-    }};
+fn test_ln_text(tokens: &[TokenLine<&str>], text: &str) {
+    title("source of each TokenLine");
+    let received: Vec<_> = tokens
+        .iter()
+        .flat_map(TokenLine::all_src_text)
+        .copied()
+        .collect();
+    let expected: Vec<_> = text.lines().collect();
+    assert_eq!(&received, &expected);
 }
 
 #[test]
@@ -35,7 +31,7 @@ fn hello_world() {
     let text = include_str!("fixtures/hello-world.egg");
     let tokens: Vec<_> = dbg!(Scan::new(text).collect());
     test_snapshot!(tokens, "snapshots/token-scan/hello-world.txt");
-    test_ln_text!(tokens, text);
+    test_ln_text(&tokens, text);
 }
 
 #[test]
@@ -43,5 +39,5 @@ fn multi_line() {
     let text = include_str!("fixtures/multi-line.egg");
     let tokens: Vec<_> = dbg!(Scan::new(text).collect());
     test_snapshot!(tokens, "snapshots/token-scan/multi-line.txt");
-    test_ln_text!(tokens, text);
+    test_ln_text(&tokens, text);
 }
