c3e924900ec50b252c13268c3eaffeda598f3b98
khai96_ <hvksmr1996@gmail.com>
2023-05-25 21:13:33 +0700

    api: Add Program to SyntaxTree
    wip: AST

--- egg-ast/src/syntax_tree.rs
+++ egg-ast/src/syntax_tree.rs
@@ -2,6 +2,8 @@ mod program;
 
 pub use program::*;
 
+use derive_more::{AsMut, AsRef, Deref, DerefMut, From, Into};
+
 /// Abstract syntax tree.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SyntaxTree {}
+#[derive(Debug, Clone, PartialEq, Eq, AsMut, AsRef, Deref, DerefMut, From, Into)]
+pub struct SyntaxTree(pub Program);
