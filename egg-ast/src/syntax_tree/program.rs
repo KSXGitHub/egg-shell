8c9c00eb301811010a2706510acb03ef9f2c4a19
khai96_ <hvksmr1996@gmail.com>
2023-05-25 21:15:19 +0700

    api: Expose fields of Program
    wip: AST

--- egg-ast/src/syntax_tree/program.rs
+++ egg-ast/src/syntax_tree/program.rs
@@ -2,8 +2,8 @@ use crate::Span;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Program {
-    span: Span,
-    body: Vec<ProgramItem>,
+    pub span: Span,
+    pub body: Vec<ProgramItem>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
