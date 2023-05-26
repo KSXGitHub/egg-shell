6a6840d32b0b3efcdec1807043f71fb1cbe000dd
khai96_ <hvksmr1996@gmail.com>
2023-05-26 23:29:48 +0700

    api: ModuleHeader
    wip: AST

--- egg-ast/src/syntax_tree/module.rs
+++ egg-ast/src/syntax_tree/module.rs
@@ -4,7 +4,13 @@ use crate::{Annotation, Span};
 pub struct Module {
     pub span: Span,
     pub annotations: Vec<Annotation>,
+    pub header: ModuleHeader,
     pub body: Vec<ModuleItem>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ModuleHeader {
+    pub span: Span,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
