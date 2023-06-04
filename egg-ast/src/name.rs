850320de78b2cf64e6a90e2bf2ebd301a1990e7a
khai96_ <hvksmr1996@gmail.com>
2023-06-04 12:12:24 +0700

    api: Rename Name::{language -> namespace}
    wip: AST

--- egg-ast/src/name.rs
+++ egg-ast/src/name.rs
@@ -1,10 +1,8 @@
 /// Resolved name from an identifier.
-///
-/// Multiple syntaxes/languages require multiple different naming conventions.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Name<Language, Value> {
-    /// Code of the target language or syntax flavor.
-    pub language: Language,
+pub struct Name<Namespace, Value> {
+    /// Code of the target namespace or language.
+    pub namespace: Namespace,
     /// Value of the identifier.
     pub value: Value,
 }
