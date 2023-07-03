8826ae9007cc4a91040cd86afd9e6524bb3abf6f
khai96_ <hvksmr1996@gmail.com>
2023-07-03 15:58:13 +0700

    api: MultiSegmentStringLiteral::separator
    wip: AST

--- egg-ast/src/syntax_tree/literal/multi_segment_string.rs
+++ egg-ast/src/syntax_tree/literal/multi_segment_string.rs
@@ -3,5 +3,6 @@ use crate::{SingleSegmentStringLiteral, Span};
 #[derive(Debug)]
 pub struct MultiSegmentStringLiteral {
     pub span: Span,
+    pub separator: Box<str>,
     pub body: Box<[SingleSegmentStringLiteral]>,
 }
