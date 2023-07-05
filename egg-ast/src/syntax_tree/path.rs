d298926f5b2b5d5aeac138c76865a5c92f01f096
khai96_ <hvksmr1996@gmail.com>
2023-07-05 20:26:27 +0700

    api: SimplePathRoot
    wip: AST

--- egg-ast/src/syntax_tree/path.rs
+++ egg-ast/src/syntax_tree/path.rs
@@ -3,5 +3,13 @@ use crate::{Identifier, Span};
 #[derive(Debug)]
 pub struct SimplePath {
     pub span: Span,
+    pub root: Option<SimplePathRoot>,
     pub body: Box<[Identifier]>,
+}
+
+#[derive(Debug)]
+pub enum SimplePathRoot {
+    External(Box<str>),
+    CurrentPackage,
+    CurrentModule,
 }
