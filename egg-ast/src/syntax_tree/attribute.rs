fb9eb9ca21c1985e27e6862022dd168c8a85b8bb
khai96_ <hvksmr1996@gmail.com>
2023-07-06 23:37:06 +0700

    api: Attribute::arguments
    wip: AST

--- egg-ast/src/syntax_tree/attribute.rs
+++ egg-ast/src/syntax_tree/attribute.rs
@@ -1,7 +1,8 @@
-use crate::{SimplePath, Span};
+use crate::{MetaArgumentList, SimplePath, Span};
 
 #[derive(Debug)]
 pub struct Attribute {
     pub span: Span,
     pub path: SimplePath,
+    pub arguments: MetaArgumentList,
 }
