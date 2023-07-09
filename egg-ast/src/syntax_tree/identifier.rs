29e4c51c415932dc71e90df2e69c2eefac0a665f
khai96_ <hvksmr1996@gmail.com>
2023-07-09 12:28:58 +0700

    api: Replace `String` with `Cow<str>`
    wip: AST

--- egg-ast/src/syntax_tree/identifier.rs
+++ egg-ast/src/syntax_tree/identifier.rs
@@ -1,9 +1,10 @@
 use crate::{Name, OptionalName, Span};
+use std::borrow::Cow;
 
 #[derive(Debug)]
-pub struct Identifier<Body = Name<String, String>> {
+pub struct Identifier<Body = Name<Cow<'static, str>, Cow<'static, str>>> {
     pub span: Span,
     pub body: Body,
 }
 
-pub type OptionalIdentifier = Identifier<OptionalName<String, String>>;
+pub type OptionalIdentifier = Identifier<OptionalName<Cow<'static, str>, Cow<'static, str>>>;
