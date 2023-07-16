c82be120df289b3197f4f390ace176cac359323c
khai96_ <hvksmr1996@gmail.com>
2023-07-17 01:30:43 +0700

    api: DeclarativeMacro::visibility
    wip: AST

--- egg-ast/src/syntax_tree/metaprogramming/declarative_macro.rs
+++ egg-ast/src/syntax_tree/metaprogramming/declarative_macro.rs
@@ -1,9 +1,10 @@
-use crate::{Attribute, MetaParameterList, OptionalIdentifier, Span};
+use crate::{Attribute, MetaParameterList, OptionalIdentifier, Span, VisibilityModifier};
 
 #[derive(Debug)]
 pub struct DeclarativeMacro {
     pub span: Span,
     pub attributes: Box<[Attribute]>,
+    pub visibility: Option<VisibilityModifier>,
     pub identifier: Option<OptionalIdentifier>,
     pub parameter: MetaParameterList,
     // TODO
